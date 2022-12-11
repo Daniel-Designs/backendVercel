48bd8e7f3db93376257b2930b34766fd8277997a
Daniel <tonystar.dm@gmail.com>
2022-12-10 19:17:47 -0600

    PORT ENV

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./app");
 
-const port = process.env.DEVPORT;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log("Server listening on port", port);
